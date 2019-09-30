fdc88122e3989256c7f28dc89896d6cd0642bb5f
Lev on VinylCoding.ru <coding1liki@gmail.comfg>
2019-09-30 23:38:00 +0300

    Добавил скрипты для копирования configs и для перезаписи index.php

---
+++ scripts/add_to_index.php
@@ -0,0 +1,17 @@
+<?php
+
+$path = __DIR__."/../../../../index.php";
+
+$str = <<<PHP
+use CodingLiki\PhpMvc\App\App;
+
+require_once "./vendor/CodingLiki/Autoloader/autoloader.php";
+require_once "./vendor/CodingLiki/View/include/helpers.php";
+
+\$app = App::getInstance();
+
+\$app->start("app_middlewares", [ "routes.routes" ]);
+
+PHP;
+
+file_put_contents($path, $str);
---
+++ scripts/copy_folders_and_files.php
@@ -0,0 +1,8 @@
+<?php
+$cfg_path = __DIR__."/../../../../configs";
+
+$cfg_example_path = __DIR__."/../configs_example";
+
+
+rcopy($cfg_example_path, $cfg_path);
+
